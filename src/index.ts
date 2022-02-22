480af976db1cac4d95d2a6f806dea0fea1b5bee3
Piotr 'Ugz' Bielski <pb93infa@gmail.com>
2022-02-22 21:55:13 +0100

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
+import { getDiscordManager } from './managers/discordManager';
+import { getListenerManager } from './managers/listenersManager';
+
+require('dotenv').config();
+
 console.log("Bot is starting...");
+const listenerManager = getListenerManager();
+
+const discordManager = getDiscordManager(listenerManager);
+discordManager.login();
