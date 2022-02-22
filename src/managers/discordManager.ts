d004b2455b0a1223fabf4b966d46988be8bed77c
Piotr 'Ugz' Bielski <pb93infa@gmail.com>
2022-02-22 01:08:59 +0100

    implemented discordManages

--- src/managers/discordManager.ts
+++ src/managers/discordManager.ts
@@ -1,12 +1,21 @@
-import { BitFieldResolvable, Client, IntentsString } from "discord.js";
+import { ListenerManager } from './listenersManager';
+import { BitFieldResolvable, Client, Intents, IntentsString } from "discord.js";
 
-export interface IDiscordManager {
+export interface DiscordManager {
     login: () => void;
 }
 
-export const DiscordManager = (): IDiscordManager => {
-    const intents: BitFieldResolvable<IntentsString, number> = 0;
+export const getDiscordManager = (listenerManager: ListenerManager): DiscordManager => {
+    const intents: BitFieldResolvable<IntentsString, number> = listenerManager.getAllIntents();
+
     const client = new Client({intents});
+    listenerManager.runAll(client.on);
+
+    const onReadyClient = () => {
+        console.log("Discord client is ready.");
+    }
+
+    client.on("ready", onReadyClient);
     
     const login = (): void => {
         client.login(process.env.DISCORD_BOT_TOKEN);
