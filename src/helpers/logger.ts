2edd08a187e03a031b4f077ecdc7a8cfe09fd6d3
Piotr 'Ugz' Bielski <pb93infa@gmail.com>
2022-02-25 01:19:39 +0100

    added Login state

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,11 +1,20 @@
-export const logText = (msg: string) => {
+export enum LoggerState {
+    fatal,
+    error,
+    warn,
+    info,
+    debug,
+    trace
+}
+
+export const logText = (msg: string, state: LoggerState = LoggerState.info) => {
     const date = new Date();
     if (process.env.SHOW_LOGS_IN_CONSOLE === "true")
-        console.log(`[${date.toLocaleString()}]: ${msg}`);
+        console.log(`[${date.toLocaleString()}]:[${LoggerState[state]}] ${msg}`);
 };
 
-export const logObject = (obj: object) => {
+export const logObject = (obj: object, state: LoggerState = LoggerState.info) => {
     const date = new Date();
     if (process.env.SHOW_LOGS_IN_CONSOLE === "true")
-        console.log(`[${date.toLocaleString()}]: ${JSON.stringify(obj)}`);
+        console.log(`[${date.toLocaleString()}]:[${LoggerState[state]}] ${JSON.stringify(obj)}`);
 };
