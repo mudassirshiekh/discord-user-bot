b35142e45b5f2b335d9061bfb7738e0e7394c6a4
Piotr 'Ugz' Bielski <pb93infa@gmail.com>
2022-03-07 18:45:45 +0100

    added month enum with polish names

---
+++ src/consts/monthEnum.ts
@@ -0,0 +1,14 @@
+export enum MonthEnum {
+    Styczeń = 0,
+    Luty,
+    Marzec,
+    Kwiecień,
+    Maj,
+    Czerwiec,
+    Lipiec,
+    Sierpień,
+    Wrzesień,
+    Październik,
+    Listopad,
+    Grudzień
+}
