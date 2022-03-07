fc0edbe78903802d0a4044e3631ee0a8a66d2b78
Piotr 'Ugz' Bielski <pb93infa@gmail.com>
2022-03-07 19:19:49 +0100

    implemented usage of pay reminder with mailing manager

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { loadFile } from './helpers/loadFile';
 import { getStatisticTextListener } from './listeners/statisticTextListener';
 import { getDiscordClientExecutors } from './executors/discordClientExecutor';
 import { createLog } from './helpers/logger';
@@ -7,6 +8,9 @@ import { getDiscordManager } from './managers/discordManager';
 import { getListenerManager } from './managers/listenersManager';
 import { getTriggerManager } from './managers/triggerManager';
 import { getPayReminderMsgTrigger } from './triggers/payReminderMsgTrigger';
+import { getMailingManager } from './managers/mailingManager';
+import { getMailingExecutor } from './executors/mailingExecutor';
+import { getPayReminderMailTrigger } from './triggers/payReminderMailTrigger';
 
 require('dotenv').config();
 
@@ -18,7 +22,11 @@ listenerManager.registerListener(getWaveHelloListener());
 const discordManager = getDiscordManager(listenerManager);
 discordManager.login();
 
+const mailingManager = getMailingManager();
+
 const triggerManagger = getTriggerManager();
 triggerManagger.registerExecutor(getDiscordClientExecutors(discordManager.getDiscordClient()));
+triggerManagger.registerExecutor(getMailingExecutor());
 triggerManagger.registerTrigger(getPayReminderMsgTrigger());
+triggerManagger.registerTrigger(getPayReminderMailTrigger());
 triggerManagger.runAll();
